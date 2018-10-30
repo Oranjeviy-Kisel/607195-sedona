f97a8cd0565df342548cf536858ddccbda2598af
Oranjeviy-Kisel <icosagon@rambler.ru>
2018-10-30 16:38:23 +0300

    Исправления для второй проверки

--- source/js/script.js
+++ source/js/script.js
@@ -5,22 +5,15 @@ showMenu.classList.remove("main-nav__list--noscript");
 
 toggleNav.addEventListener("click", function (evt) {
   evt.preventDefault();
-
-
   if (this.classList.contains("main-nav__switcher--hamburger")) {
     this.classList.remove("main-nav__switcher--hamburger");
     this.classList.add("main-nav__switcher--closer");
-
     showMenu.classList.add("main-nav__list--noscript");
     console.log(showMenu);
   } else {
     this.classList.remove("main-nav__switcher--closer");
     this.classList.add("main-nav__switcher--hamburger");
-
     showMenu.classList.remove("main-nav__list--noscript");
     console.log(showMenu);
   };
-
-//popup.classList.add("modal-show");
-//usrname.focus();
 });
